fcd87a17dddbbab334d441850d8c61b014ea518a
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-09 13:44:19 +0530

    Add files via upload

---
+++ Data_Types/Size.py
@@ -0,0 +1,11 @@
+import sys
+
+No = 9497379929238094920348374203874730298347       # int
+
+Marks = 90.90       # float
+
+Name = "Rahul"      # str
+
+print(sys.getsizeof(No))
+print(sys.getsizeof(Marks))
+print(sys.getsizeof(Name))
