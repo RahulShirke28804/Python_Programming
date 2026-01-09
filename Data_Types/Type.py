cac121cb41f8bf8dbcbaf663fa538b7b41b66c76
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-09 13:59:21 +0530

    Add docstring to Type.py for clarity
    Added a docstring to explain the purpose of the program.

--- Data_Types/Type.py
+++ Data_Types/Type.py
@@ -1,3 +1,9 @@
+'''
+    This program demonstrates how to check the data type of different variables 
+    like int, float, str, bool, and complex.
+'''
+
+
 Age = 35            # int 
 Marks = 90.89       # float
 City = "Pune"       # str
