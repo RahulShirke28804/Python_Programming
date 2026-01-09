9c7e87fc6da9cfa697704ac9c52d5c231411a676
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-09 14:00:07 +0530

    Update Size.py

--- Data_Types/Size.py
+++ Data_Types/Size.py
@@ -1,3 +1,8 @@
+'''
+    This program uses the sys module to check the memory size (in bytes) 
+    occupied by different data types such as int, float, and str.
+'''
+
 import sys
 
 No = 9497379929238094920348374203874730298347       # int
