e3509f230c7f0e45c2879494a12f35fdb601fce2
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-09 14:02:15 +0530

    Document dictionary usage in Mapping.py
    Add docstring to explain dictionary usage

--- Data_Types/Mapping.py
+++ Data_Types/Mapping.py
@@ -1,3 +1,8 @@
+'''
+    This program demonstrates a dictionary storing data 
+    as key–value pairs and accessing values using keys.
+'''
+
 Information = {"Name" : "Rahul", "Age" : 25, "City" : "Pune", "Marks" : 89.90}
 print(type(Information))
 print(Information)
