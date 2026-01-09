9d8f5361297037e758adc9b3928694af9eba8541
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-09 14:02:44 +0530

    Update MappingX.py

--- Data_Types/MappingX.py
+++ Data_Types/MappingX.py
@@ -1,3 +1,9 @@
+'''
+    This program shows that dictionary values can be modified 
+    by assigning a new value to an existing key.
+'''
+
+
 Information = {"Name" : "Rahul", "Age" : 25, "City" : "Pune", "Marks" : 89.90}
 print(type(Information))
 print(Information)
