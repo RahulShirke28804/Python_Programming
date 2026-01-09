1cb9f72027d89522bf80d3bb63ff5215d1a39e62
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-09 13:48:48 +0530

    Add files via upload

---
+++ Data_Types/BinaryXX.py
@@ -0,0 +1,17 @@
+'''
+    This program demonstrates bytearray, which is similar to bytes 
+    but mutable, allowing modification using indexing.
+'''
+
+# Indexed
+# Ordered
+# Mutable
+
+Data = bytearray([65,97,98])
+
+print(Data)
+print(type(Data))
+print(Data[0])
+
+Data[0] = 66
+print(Data[0])
