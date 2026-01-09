07ee70690c83d9cd0bb120d2e468fe4cb063b327
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-09 13:45:41 +0530

    Add user input program for name and age
    This program prompts the user for their name and age, then prints the input values.

---
+++ Input_Output/IO.py
@@ -0,0 +1,14 @@
+'''
+    This program accepts the user’s name and age as input and prints them.
+    All input taken using input() is stored as a string.
+'''
+
+print("Enter your name : ")
+Name = input()
+
+print("Hello ",Name)
+
+print("Enter your age : ")
+Age = input()
+
+print("Your age is : ",Age)
