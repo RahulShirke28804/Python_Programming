1930e78aa2ff6337deef95d085a1deb34743ed26
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-12 12:27:20 +0530

    Add files via upload

---
+++ Data_Types/Range_1.py
@@ -0,0 +1,16 @@
+'''
+    This code shows how the range() function works in Python. 
+    It creates a range of numbers, prints the range object itself, 
+    and displays its data type. It also demonstrates a range with 
+    a custom start, stop, and step value.
+'''
+
+data = range(7)
+
+print(data)
+print(type(data))
+
+test = range(3,9,2)
+
+print(test)
+print(type(test))
