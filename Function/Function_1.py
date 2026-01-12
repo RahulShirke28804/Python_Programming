d18d2eec6b2e78d2e114fbbfa1148f6a234c45a1
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-12 12:49:41 +0530

    Add files via upload

---
+++ Function/Function_1.py
@@ -0,0 +1,11 @@
+'''
+    This program assigns two numeric values to variables, multiplies them, and prints the result. 
+    It demonstrates simple arithmetic operations and variable usage in Python.
+'''
+
+No1 = 10
+No2 = 11
+
+Ans = No1 * No2
+
+print("Multiplication is : ",Ans)
