16511b59cb1066e7bc1eb312739a921d94cfccd3
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-12 13:00:45 +0530

    Add files via upload

---
+++ Function/FunctionDemo13.py
@@ -0,0 +1,14 @@
+
+def Phoenix():
+    print("Inside Phoenix")
+    
+    def Zara():
+        print("Inside Zara")
+    
+    Zara()
+
+def main():
+        Phoenix()
+
+if __name__ == "__main__":
+    main()
