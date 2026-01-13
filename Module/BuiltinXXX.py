0eb8a1111a4d4352c1443a97c6d3f8dd3f05464d
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-13 12:17:46 +0530

    Add files via upload

---
+++ Module/BuiltinXXX.py
@@ -0,0 +1,10 @@
+'''
+    Module Alias Usage
+
+Shows how to create an alias for a module using as to shorten references.
+'''
+
+import math as m 
+result = m.sqrt(16)
+
+print("Square root is : ",result)
