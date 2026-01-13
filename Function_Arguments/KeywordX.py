eb4ab23ebabea33fab8f402609af893a40e738b5
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-13 12:15:05 +0530

    Add files via upload

---
+++ Function_Arguments/KeywordX.py
@@ -0,0 +1,18 @@
+'''
+    Keyword Arguments with None
+
+Shows that keyword arguments can explicitly receive None as a valid value without causing errors.
+'''
+
+def EmployeeInfo(Name, Age, Salary, City):
+    print("Name : ",Name)
+    print("Age : ",Age)
+    print("Salary : ",Salary)
+    print("City : ",City)
+
+def main():
+    # Keyword arguments
+    EmployeeInfo(Age=26,Name="Rahul",City="Pune",Salary=None)     # Correct
+
+if __name__ == "__main__":
+    main()  
