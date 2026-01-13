e3947f23aa0294bc307c8c16d194cd402f66ad1f
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-13 12:17:04 +0530

    Add files via upload

---
+++ Module/BuiltinXX.py
@@ -0,0 +1,11 @@
+'''
+    Importing All Functions from a Module
+
+Demonstrates importing all public functions from the math module using the wildcard *.
+'''
+
+from math import *  
+
+result = sqrt(16)
+
+print("Square root is : ",result)
