98fa7963208fbb86903855261e6987d69a1fe14d
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-13 12:16:24 +0530

    Create Builtin.py

---
+++ Module/Builtin.py
@@ -0,0 +1,11 @@
+'''
+    Using math Module (Full Import)
+
+Demonstrates importing the entire math module and accessing its functions using the module name.
+'''
+
+import math
+
+result = math.sqrt(16)
+
+print("Square root is : ",result)
