3b361b8984a3ba0078966f6fe8fa52e4ebeb2eae
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-13 12:16:44 +0530

    Add files via upload

---
+++ Module/BuiltinX.py
@@ -0,0 +1,11 @@
+'''
+    Importing Specific Function from Module
+
+Shows how to import a specific function (sqrt) directly from a module for cleaner syntax.
+'''
+
+from math import sqrt
+
+result = sqrt(16)
+
+print("Square root is : ",result)
