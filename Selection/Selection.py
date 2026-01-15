28d8bedc0bdd4ff4443a445281a94df2e3ac62b2
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-15 14:43:24 +0530

    Add selection logic to compare two numbers
    Implement a simple comparison between two numbers.

---
+++ Selection/Selection.py
@@ -0,0 +1,13 @@
+'''
+    Demonstrates the use of a relational operator by comparing two numbers and printing the boolean result. 
+    
+'''
+
+no1 = 11
+no2 = 21
+
+if (no1 > no2):
+    print("no1 is greater")
+else:
+    print("no2 is greater")
+    
