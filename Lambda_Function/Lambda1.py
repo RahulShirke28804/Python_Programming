413fef0eaa591fd8a9953247bad127a5588d4224
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-15 14:42:37 +0530

    Create Lambda1.py

---
+++ Lambda_Function/Lambda1.py
@@ -0,0 +1,10 @@
+'''
+    Demonstrates the use of a lambda function to perform addition of two numbers and print the result. 
+'''
+
+Add = lambda No1,No2 : No1 + No2
+
+Ret = Add(11,10)
+# Ret = 11 + 10
+
+print("Adition is : ",Ret)
