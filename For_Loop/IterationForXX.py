2a6b91fbac7525502f3ace44759d506d201ba93f
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-15 21:39:15 +0530

    Add files via upload

---
+++ For_Loop/IterationForXX.py
@@ -0,0 +1,15 @@
+# Sequence
+'''
+    Compares sequential statements with iteration using a for loop to print repeated output efficiently.
+'''
+print("Jay Ganesh")
+print("Jay Ganesh")
+print("Jay Ganesh")
+print("Jay Ganesh")
+
+print("----------------------------------------------------")
+
+# Iteration
+
+for i in range(4):
+    print("Jay Ganesh")
