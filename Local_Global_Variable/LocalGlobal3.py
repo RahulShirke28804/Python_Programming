33d762059ed755ea5afb3cfe51eab8cc4b12542a
RahulShirke28804 <rahulshirke28804@gmail.com>
2026-01-16 12:59:05 +0530

    Add files via upload

---
+++ Local_Global_Variable/LocalGlobal3.py
@@ -0,0 +1,15 @@
+'''
+    Demonstrates local variable scope where changes inside a function do not affect the global variable.
+'''
+
+No = 11         # Global
+
+def Fun():
+    No = 21
+    print("Value of No from Fun is : ",No)      # 21
+    No = No + 1
+    print("Value of No from Fun is : ",No)      # 22
+
+print("Value of No is : ",No)                   # 11
+Fun()
+print("Value of No is : ",No)                   # 11
